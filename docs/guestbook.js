502ffe5e5fcec555385bedc12b853d67b9af8986
Marco Antonio Alvarez Guzman <marcoalvarezguzman@Marcos-MacBook-Pro.local>
2020-01-28 17:44:43 -0600

    nuevocommit

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -3,7 +3,7 @@
  * aplicacion serverless natural...
  * pagina guestbook
  */
-const apiUrl = 'https://2166d93d.us-east.apiconnect.appdomain.cloud/7fdb8107-a76d-4275-830d-019450118c56/teacherAdvocates';
+const apiUrl = 'https://a22369f6.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
